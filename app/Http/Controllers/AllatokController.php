9383f67505dddd90ea9323e3a03de74796bfaacb
KannaGH <kamlah.gabor@gmail.com>
2023-12-21 22:59:22 +0100

    A menhelyProfil-on ha nincs még állat akkor az allatregisztracio-ra dob

--- app/Http/Controllers/AllatokController.php
+++ app/Http/Controllers/AllatokController.php
@@ -114,7 +114,7 @@ Az authorize hívás során a megfelelő argumentumokat használva a záradékba
 
        
        // Ellenőrizzük, hogy a bejelentkezett felhasználó "menhely" típusú-e
-    if (auth()->check() && auth()->user()->type === 2) {
+    if (auth()->check() && auth()->user()->type === 1) {
         $email = auth()->user()->email;
         $menhely = Menhely::where('email', $email)->first();
         if ($menhely) {
