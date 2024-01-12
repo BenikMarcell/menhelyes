b750d96fad7ec2e67c5056d60b824556ff0a7428
KannaGH <kamlah.gabor@gmail.com>
2024-01-12 22:41:16 +0100

    CSS Menhely profil oldal OK

--- resources/views/menhelyProfil.blade.php
+++ resources/views/menhelyProfil.blade.php
@@ -27,48 +27,48 @@
             use App\Models\Menhely;
             $email = auth()->user()->email;
             $menhely = Menhely::where('email', $email)->first();
-            
             ?>
 
             <h1 class="text-center mb-5">{{ Auth::user()->name }}</h1>
             <!--<h4 class="text-center mb-5">{{ Auth::user()->id }}</h4>-->
             <!-- ide először beolvassuk, hogy melyik menhelről van szó
-            aztán kivesszük a menhelyből az adatait és azokat írjuk alább.
-            A képfeltöltés még hiányzik!
-            -->
-            <div class="col-6">
-                @if ($menhely->kep)
-                    <img class="img-fluid" src="{{ asset('storage/images/' . $menhely->kep->src) }}"
-                        alt="Próba {{ $menhely->kep->src }}">
-                @endif
-            </div>
+                        aztán kivesszük a menhelyből az adatait és azokat írjuk alább.
+                        A képfeltöltés még hiányzik!
+                        -->
+            <div class="row">
+                <div class="col-12 col-md-6">
+                    @if ($menhely->kep)
+                        <img class="img-fluid mb-5" src="{{ asset('storage/images/' . $menhely->kep->src) }}"
+                            alt="Próba {{ $menhely->kep->src }}">
+                    @endif
+                </div>
 
-            <div class="col-6">
-                <h3>Leírás:</h3>
+                <div class="col-12 col-md-6">
+                    <h3 class="mb-3">Leírás:</h3>
+                    <p>{{ $menhely->menhely_leiras }}</p>
 
+                    <div>
+                        <h4 class="mb-3">Adatok:</h4>
+                        <div class="col">
+                            <p>Irányítószám: {{ $menhely->irsz }}</p>
+                            <p>Menhely neve: {{ $menhely->nev }}</p>
+                            <p>Település: {{ $menhely->telepules }}</p>
+                            <p>Utca, házszám: {{ $menhely->utca_hsz }}</p>
+                            <p>Bankszámla: {{ $menhely->bankszamla_sz }}</p>
+                            <p>Nyilvántartási szám: {{ $menhely->nyilv_szam }}</p>
+                            <p>Adószám: {{ $menhely->adoszam }}</p>
+                            <p>Email: {{ $menhely->email }}</p>
+                            <p>Telefon: {{ $menhely->telefon }}</p>
+                            <p>Web-oldal: {{ $menhely->weblink }}</p>
+                        </div>
+                    </div>
+                </div>
 
-                <p>{{ $menhely->menhely_leiras }}</p>
             </div>
         </div>
 
         <div class="row">
-            <div class="col-6">
-                <h4>Adatok:</h4>
-                <div class="col">
-                    <p>Irányítószám: {{ $menhely->irsz }}</p>
-                    <p>Menhely neve: {{ $menhely->nev }}</p>
-                    <p>Település: {{ $menhely->telepules }}</p>
-                    <p>Utca, házszám: {{ $menhely->utca_hsz }}</p>
-                    <p>Bankszámla: {{ $menhely->bankszamla_sz }}</p>
-                    <p>Nyilvántartási szám: {{ $menhely->nyilv_szam }}</p>
-                    <p>Adószám: {{ $menhely->adoszam }}</p>
-                    <p>Email: {{ $menhely->email }}</p>
-                    <p>Telefon: {{ $menhely->telefon }}</p>
-                    <p>Web-oldal: {{ $menhely->weblink }}</p>
-                </div>
-
-            </div>
-            <div class="col-6">
+            <div class="col-12-md-6">
                 <a href="{{ route('menhely.edit') }}" class="btn">Menhely profil szerkesztése</a>
             </div>
         </div>
