2188d6d5b939c488728475855a3f01276f89cd76
KannaGH <kamlah.gabor@gmail.com>
2024-01-12 14:04:00 +0100

    Elkezdtem rendbetenni a CSS-t

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,35 +11,37 @@
 @endphp
 
 
-<h1 class="text-center">Főoldal</h1>
+<h1 class="text-center m-3 kekText">Üdvözlünk a MenhelyHUB-on</h1>
 
 <div class="container">
     <div class="row">
         <div class="col-12 col-md-6">
-            <img id="fooldalkep" src="./images/macska2.jpg" alt="fooldalkep" class="img-fluid">
+            <img id="fooldalkep" src="./images/Welcome_Image.jpg" alt="fooldalkep" class="img-fluid">
         </div>
         <div class="col-12 col-md-6">
-            <h2 class="text-center">A menhely HUB célja</h2>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia omnis minus dolor, ratione quia perferendis. Animi neque dolore, consequatur, debitis soluta voluptatum ipsam esse obcaecati aut, sit quae voluptates fugiat!
+            <h2 class="text-center mb-3" >A MenhelyHUB célja</h2>
+            <p class="mx-3">A Menhely HUB egy olyan weboldal ami összegyűjti és egyszerre mutatja a különböző menhelyek állatait a befogadni szándékozó felhasználók számára.
+A menhelyek egy regisztrációval részesei lehetne a rendszernek, feltölthetik az állataikat és adományokat fogadhatnak.</p>
+
         </div>
     </div> <!-- </div> hozzáadva itt -->
 </div>
 
 <!-- Állat kártyák -->
 <div class="container">    
-    <div class="row">
-        <h2>Néhány állatka:</h2>
+    <div class="row justify-content-center">
+        <h2 class="m-3">Néhány állatka:</h2>
         @foreach ($allatok as $key => $allat)
-            <div class="col-md-3  allat-card">
+            <div class="col-12 col-md-4 menhely-card">
                 <div class="card mb-2">
                         @if($allat->kep)
-                            <img class="card-img-top myCardImg" src="{{ asset('storage/images/' . $allat->kep->src) }}" alt="Próba {{ $allat->kep->src }}"> 
+                            <img class="card-img-top" src="{{ asset('storage/images/' . $allat->kep->src) }}" alt="Próba {{ $allat->kep->src }}"> 
                         @endif
                         <div class="card-body">
                         <h4 class="card-title">{{$allat->nev}}</h4>
                         <h6>{{$allat->kor}} hónapos</h6>
                         <h6>{{$allat->nem}}</h6>
-                        <a href="{{ route('egyAllat', ['a_id' => $allat->a_id]) }}" class="btn rounded">Bővebben</a>
+                        <a href="{{ route('egyAllat', ['a_id' => $allat->a_id]) }}" class="btn rounded">Ismerj meg!</a>
                         
                     </div>
                 </div>
@@ -50,19 +52,19 @@
 
 <!-- Menhely kártyák -->
 <div class="container">
-    <div class="row">
-        <h2>Állat menhely partnereink:</h2>
+    <div class="row justify-content-center">
+        <h2 class="m-3">Állatmenhely partnereink:</h2>
         @foreach ($menhelyek as $key => $menhely)
-        <div class="col-md-3  menhely-card">
+        <div class="col-12 col-md-4 menhely-card">
             <div class="card mb-2">
                         @if($menhely->kep)
-                        <img class="card-img-top myCardImg" src="{{ asset('storage/images/' . $menhely->kep->src) }}" alt="Próba {{ $menhely->kep->src }}">
+                        <img class="card-img-top " src="{{ asset('storage/images/' . $menhely->kep->src) }}" alt="Próba {{ $menhely->kep->src }}">
                         @endif
                         <div class="card-body">
                         <h4 class="card-title">{{$menhely->nev}}</h4>
                         <h6>{{$menhely->telepules}}</h6>
                         <h6>{{$menhely->telefon }}</h6>
-                        <a href="{{ route('egyMenhely', ['m_id' => $menhely->m_id]) }}" class="btn rounded">Bővebben</a>
+                        <a href="{{ route('egyMenhely', ['m_id' => $menhely->m_id]) }}" class="btn rounded">Részletes adatok</a>
                     </div>
                 </div>
             </div>
